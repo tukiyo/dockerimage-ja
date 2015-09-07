0cae16d4fc938107b0695c3bf198aec55681a08f
tukiyo3 <tukiyo3@gmail.com>
2015-09-07 12:12:02 +0900

    lodgeの初期アカウント追加できた

--- docker-backup.sh
+++ docker-backup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-set -eux
+set -eu
 if [ ! $# -eq 3 ];then
   echo "$0 CONTAINER_NAME tar_name DIRECTORY"
   exit 1
 fi
 
+set -x
 docker run --rm \
   --volumes-from $1 \
   -v $(pwd):/backup \
   busybox tar cf /backup/$2 $3
-# busybox tar cf /backup/app.tar /app
--- lodge/README.md
+++ lodge/README.md
@@ -1,1 +1,6 @@
 port 3000
+
+## default account
+
+* email: admin@localhost.localdomain
+* pass: changeme!
